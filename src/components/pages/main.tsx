dbc4ad45fd070ba054f3a589536d75a1e195d4d1
Kabir Bose <kabirbose04@gmail.com>
2024-03-26 21:18:31 -0400

    created main page

---
+++ src/components/pages/main.tsx
@@ -0,0 +1,12 @@
+"use client";
+
+const Main = () => {
+  return (
+    <div className="bg-slate-950 text-slate-300 flex flex-col justify-center items-center h-screen">
+      <h1 className="text-4xl font-semibold">Kabir Bose</h1>
+      <p className="text-xl font-md">Software Eng & Cybersecurity</p>
+    </div>
+  );
+};
+
+export default Main;
