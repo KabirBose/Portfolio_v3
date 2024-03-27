762054dbd24d5caf5d89fcb57c00b04f8355755d
Kabir Bose <kabirbose04@gmail.com>
2024-03-27 09:41:24 -0400

    navbar res created

--- src/components/pages/main.tsx
+++ src/components/pages/main.tsx
@@ -2,8 +2,8 @@
 
 const Main = () => {
   return (
-    <div className="bg-slate-900 text-slate-300 flex flex-col justify-center items-center h-screen gap-3">
-      <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold">
+    <div className="bg-zinc-950 h-[35rem] flex flex-col justify-center items-center gap-3">
+      <h1 className="text-4xl md:text-5xl lg:text-6xl font-semibold text-blue-500">
         Kabir Bose
       </h1>
       <p className="text-xl md:text-2xl lg:text-3xl font-md">
--- src/components/ui/navbar/navbar.tsx
+++ src/components/ui/navbar/navbar.tsx
@@ -1,12 +1,61 @@
+"use client";
+
 import Link from "next/link";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import { useState } from "react";
 
 const Navbar = () => {
+  const [opened, setOpened] = useState(false);
+
+  const navActions = function () {
+    setOpened(!opened);
+  };
+
   return (
-    <nav className="text-slate-300 bg-slate-900 flex justify-center items-center p-7 gap-8 text-lg md:text-xl">
-      <Link href={"/"}>About</Link>
-      <Link href={"/"}>Skills</Link>
-      <Link href={"/"}>Certifications</Link>
-      <Link href={"/"}>Contact</Link>
+    <nav className="bg-zinc-950 p-7 text-lg md:text-xl">
+      <div className="hidden justify-center items-center gap-8 md:flex">
+        <Link className="hover:text-blue-500" href={"/"}>
+          About
+        </Link>
+        <Link className="hover:text-blue-500" href={"/"}>
+          Skills
+        </Link>
+        <Link className="hover:text-blue-500" href={"/"}>
+          Certifications
+        </Link>
+        <Link className="hover:text-blue-500" href={"/"}>
+          Contact
+        </Link>
+      </div>
+
+      <div className="md:hidden">
+        <FontAwesomeIcon
+          icon={faBars}
+          size="lg"
+          style={{ color: "#ffffff" }}
+          onClick={() => navActions()}
+          className="cursor-pointer"
+        />
+        <div
+          className={`${
+            opened ? "mt-5 absolute flex flex-col gap-5" : "hidden"
+          }`}
+        >
+          <Link className="hover:text-blue-500" href={"/"}>
+            About
+          </Link>
+          <Link className="hover:text-blue-500" href={"/"}>
+            Skills
+          </Link>
+          <Link className="hover:text-blue-500" href={"/"}>
+            Certifications
+          </Link>
+          <Link className="hover:text-blue-500" href={"/"}>
+            Contact
+          </Link>
+        </div>
+      </div>
     </nav>
   );
 };
