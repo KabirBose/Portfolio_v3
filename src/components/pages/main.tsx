90e463c57124af3c2a1bf0405cc00b5acd907090
Kabir Bose <kabirbose04@gmail.com>
2024-03-26 21:44:21 -0400

    fixed viewport res

--- src/components/pages/main.tsx
+++ src/components/pages/main.tsx
@@ -2,9 +2,13 @@
 
 const Main = () => {
   return (
-    <div className="bg-slate-950 text-slate-300 flex flex-col justify-center items-center h-screen">
-      <h1 className="text-4xl font-semibold">Kabir Bose</h1>
-      <p className="text-xl font-md">Software Eng & Cybersecurity</p>
+    <div className="bg-slate-950 text-slate-300 flex flex-col justify-center items-center h-screen gap-3">
+      <h1 className="text-5xl md:text-6xl lg:text-8xl font-semibold">
+        Kabir Bose
+      </h1>
+      <p className="text-2xl md:text-3xl lg:text-5xl font-md">
+        Software Eng & Cybersecurity
+      </p>
     </div>
   );
 };
---
+++ src/components/ui/navbar/navbar.tsx
@@ -0,0 +1,12 @@
+const Navbar = () => {
+  return (
+    <nav className="bg-slate-950 text-slate-300 flex justify-center items-center gap-5 p-7 text-lg md:text-2xl lg:text-3xl md:gap-12">
+      <p>About</p>
+      <p>Skills</p>
+      <p>Certifications</p>
+      <p>Contact</p>
+    </nav>
+  );
+};
+
+export default Navbar;
